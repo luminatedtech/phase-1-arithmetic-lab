aa60be00da2e93b8c7543ba68ce01dc4d6100a8d
avalmercafe02@gmail.com <allenkim@Allens-MacBook-Pro.local>
2022-05-20 13:26:47 -0400

    commit tree d88b75af66c7593d5e40db745cd5dc15f83b25dc

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+const multiply = num1*num2 
+
+
+const random = Math.floor(Math.random()*100+1)
+console.log(random)
+
+const num3 = 7
+const num4 = 32
+
+const mod = num4 % num3
+
+const max = Math.max(1,2,3,4,5,20)
